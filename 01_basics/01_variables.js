92468b2c66e6b88a377b35e5c25f061feb4fa649
Suyash2222 <127912788+Suyash2222@users.noreply.github.com>
2025-02-24 06:04:51 +0000

    understanding_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1456;
+let accountHolderName="Suyash"
+var accountEmail="suyash@google.com"
+
+let accountState;
+
+// accountId=528; constant variables can not be changed
+accountHolderName="Yash"
+accountEmail="yash@gmail.com"
+
+accountCity="Pune"
+console.table([accountId,accountHolderName,accountEmail,accountCity,accountState]);
