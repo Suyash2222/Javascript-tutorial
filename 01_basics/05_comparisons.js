60003174f83107de9cbb25dc685352c4be5e72e2
Suyash2222 <127912788+Suyash2222@users.noreply.github.com>
2025-02-25 05:16:48 +0000

    comparisons

---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// // console.log(null > 0);
+// // console.log(null == 0);
+// // console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// console.log(undefined >= 0);
+
+// Avoid this null and undefined type comparison
+
+
+
