963558f094dbb8a4b2aab8e5938d6059338f1e19
Suyash2222 <127912788+Suyash2222@users.noreply.github.com>
2025-02-26 07:41:55 +0000

    objects and methods of objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+// const obj= new Object();//singleton object
+// console.log(obj);
+
+const AppUser={
+    email:"elie@max.com",
+    UserDetails:{
+        fullName:{
+            FirstName:"Elizabeth",
+            LastName:"Maxihoff"
+        }
+    }
+}
+
+// console.log(AppUser);
+
+
+// console.log(AppUser.UserDetails.fullName.FirstName);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+
+// const obj3=Object.assign({},obj1,obj2)
+//The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. 
+// It returns the modified target object.
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+
+console.log(Object.keys(AppUser));
+console.log(Object.values(AppUser));
+console.log(Object.entries(AppUser));
+
+
