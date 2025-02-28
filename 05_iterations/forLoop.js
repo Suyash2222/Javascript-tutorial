d5867eb152e086977f3868669ab3811e6dd17bfa
Suyash2222 <127912788+Suyash2222@users.noreply.github.com>
2025-02-28 06:54:08 +0000

    for loop

---
+++ 05_iterations/forLoop.js
@@ -0,0 +1,45 @@
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+    
+// }
+
+/*for (let i = 1; i <= 10; i++) {
+    console.log(`Table Of ${i}`);
+    
+    for (let j = 1; j <=10; j++) {
+        console.log(`${i} * ${j}= `,i*j);
+        
+        
+    } 
+}*/
+
+//break and continue
+
+// for (let i = 0; i <10; i++) {
+//     if (i == 5) {
+//         console.log(`element ${i} found `);
+//         break;
+        
+//     }
+//     console.log(i);
+    
+    
+// }
+
+for (let i = 0; i <10; i++) {
+    if (i == 5) {
+        console.log(`element ${i} found `);
+        continue;
+        
+    }
+    console.log(i);
+    
+    
+}
