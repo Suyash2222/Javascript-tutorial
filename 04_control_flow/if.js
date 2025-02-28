b69c02b8b73e0d3b8aace39e88018fdf11fef0f2
Suyash2222 <127912788+Suyash2222@users.noreply.github.com>
2025-02-28 06:53:55 +0000

    conditions

---
+++ 04_control_flow/if.js
@@ -0,0 +1,38 @@
+//if
+
+// let score=200
+// if(score>100){
+//     console.log("Score is more than 100");
+//     let power="fly"
+//     console.log(`power is ${power}`);
+    
+// }
+// console.log(`${power}`);
+
+
+// let balance=1000
+// if(balance>500) console.log("test"); //implicite scope  for one line code only
+
+// const money=400
+// if (money>500) {
+//     console.log("Balance is greater than 500");
+    
+// }
+// else if(money<500)
+// {
+//     console.log("Balance is less than 500");
+    
+// }
+// else{
+//     console.log("Inssufficient");
+    
+// }
+
+const userLoggedIn= true
+const creditCard= true
+
+if(userLoggedIn && creditCard)
+{
+    console.log("Allowed");
+    
+}
